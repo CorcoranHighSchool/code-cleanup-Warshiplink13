28e232628126e69cce4670cc544e4c41677b6333
William Robert Castleman Jr <142448536+Warshiplink13@users.noreply.github.com>
2024-04-04 14:06:33 -0700

    Removed unuesd liberaries, Removed unuesd methods, Removed Public Fields.

--- Prototype2/Assets/Scripts/MoveForward.cs
+++ Prototype2/Assets/Scripts/MoveForward.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 40.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private float speed = 40.0f;
     // Update is called once per frame
     void Update()
     {

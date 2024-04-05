ed9bb00de4781643fd6f29523157871118c12d64
William Robert Castleman Jr <142448536+Warshiplink13@users.noreply.github.com>
2024-04-04 18:30:58 -0700

    Added Serializefield to animalPrefabs

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     //animal prefabs
-    public GameObject[] animalPrefabs;
+    [SerializeField]private GameObject[] animalPrefabs;
     //x range
     private float spawnRangeX = 20.0f;
     //z position
     private float spawnPosZ = 20.0f;
-
     //Start Delay
     private float startDelay = 2.0f;
     //Spawn Interval

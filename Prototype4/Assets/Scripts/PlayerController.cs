e19946c67f8647a505588c18232588c257af2d1c
William Robert Castleman Jr <142448536+Warshiplink13@users.noreply.github.com>
2024-04-09 14:43:31 -0700

    Removed un used liberies, SerializeField public property, Added Const String,

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float speed = 5.0f;
-    public GameObject focalPoint;
-    public bool hasPowerup;
-    private float powerUpStrength = 15.0f;
-    public GameObject powerupIndicator;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private GameObject focalPoint;
+    [SerializeField] private bool hasPowerup {get; private set;};
+    [SerializeField] private float powerUpStrength = 15.0f;
+    [SerializeField] private GameObject powerupIndicator;
+    private const string focalpoint = "Focal Point";
+    private const string vertical = "Vertical";
+    private const string powerup = "Powerup";
+    private const string enemy = "Enemy";
+    private const string collided with = "Collided with ";
+    private const string with powerup set to = "with powerup set to ";
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("Focal Point");
+        focalPoint = GameObject.Find(focalpoint);
     }
 
     // Update is called once per frame
     void Update()
     {
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
-        float verticalInput = Input.GetAxis("Vertical");
+        float verticalInput = Input.GetAxis(vertical);
         playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Powerup"))
+        if (other.CompareTag(powerup))
         {
             powerupIndicator.SetActive(true);   //
             hasPowerup = true;
@@ -46,13 +50,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        if (collision.gameObject.CompareTag(enemy) && hasPowerup)
         {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayfromPlayer = (collision.gameObject.transform.position - transform.position);
 
-            Debug.Log("Collided with " + collision.gameObject.name +
-                "with powerup set to " + hasPowerup);
+            Debug.Log(collided with + collision.gameObject.name +
+                with powerup set to + hasPowerup);
             enemyRigidbody.AddForce(awayfromPlayer * powerUpStrength, ForceMode.Impulse);
         }
     }

6dbfd3454bdeda6cb689e07502dbcb57b75d58ea
William Robert Castleman Jr <142448536+Warshiplink13@users.noreply.github.com>
2024-04-09 13:25:35 -0700

    Removed unused liberies, SerializeField public property, Added Const String.

--- Prototype4/Assets/Scripts/Enemy.cs
+++ Prototype4/Assets/Scripts/Enemy.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 3.0f;
+    [SerializeField] private float speed = 3.0f;
     private Rigidbody enemyRb;
     private GameObject player;
+    private const string player = "Player";
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.Find(player);
         enemyRb = GetComponent<Rigidbody>();
     }
 

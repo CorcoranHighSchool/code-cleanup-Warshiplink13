188fc86343fdb334586a379800351ee501eb1aff
William Robert Castleman Jr <142448536+Warshiplink13@users.noreply.github.com>
2024-04-09 17:18:40 -0700

    Removed Unused liberies, SerializeField public property. DifficultyButton.cs

--- Prototype5/Assets/Scripts/DifficultyButton.cs
+++ Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
     private GameManager gameManager;
-    public int difficulty;
+    [SerializeField] private int difficulty;
     // Start is called before the first frame update
     void Start()
     {
